2832eca7fdb53908a733cb177d82abd3a73d59cb
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2015-03-11 07:44:10 +0100

    Add a launcher with a main method.

---
+++ qualification-round/src/fr/mashbarth/hascode/qualificationround/Launcher.java
@@ -0,0 +1,14 @@
+package fr.mashbarth.hascode.qualificationround;
+
+
+public final class Launcher {
+
+
+    public static void main(String[] args) {
+        System.out.println("#### Qualification Round ####");
+    }
+
+    // Non instantiability.
+    private Launcher() {
+    }
+}
