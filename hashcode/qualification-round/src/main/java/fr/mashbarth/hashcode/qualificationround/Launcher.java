7a1302f9996f178a19207c093a6a5a01bfb64dc1
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2015-03-12 20:37:23 +0100

    Parse the input file and create the problem.

--- hashcode/qualification-round/src/main/java/fr/mashbarth/hashcode/qualificationround/Launcher.java
+++ hashcode/qualification-round/src/main/java/fr/mashbarth/hashcode/qualificationround/Launcher.java
@@ -14,20 +14,24 @@ import java.util.List;
 
 public final class Launcher {
 
+
+    private static final String INPUT_RESOURCE = "/dc.in";
+
+    private static final String TEST_RESOURCE = "/test.in";
+
     public static void main(String[] args) {
         System.out.println("#### Qualification Round ####");
 
-        // Example of reading a file.
-        readResource("/test-resources.txt");
+        final Problem problem = parseInput(INPUT_RESOURCE);
 
-        // Example of writing a list of object to a file.
-        final List<String> strings = new ArrayList<String>();
-        strings.add("Here");
-        strings.add("is");
-        strings.add("a");
-        strings.add("small");
-        strings.add("example !");
-        writeFile("test-result.txt", strings);
+        System.out.println("#### Racks ####");
+        System.out.println(problem.racks.toString());
+
+        System.out.println("#### Servers ####");
+        System.out.println(problem.servers.toString());
+
+        System.out.println("#### Groups ####");
+        System.out.println(problem.groups.toString());
     }
 
 
@@ -39,7 +43,6 @@ public final class Launcher {
      */
     public static void readResource(String resourceName) {
         try {
-
             final InputStream inputStream = resourceName.getClass().getResourceAsStream(resourceName);
             final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             final BufferedReader br = new BufferedReader(inputStreamReader);
@@ -86,8 +89,80 @@ public final class Launcher {
     private Launcher() {
     }
 
+    private static Problem parseInput(String inputName) {
+        Problem problem = new Problem();
+        try {
+            final InputStream inputStream = Launcher.class.getClass().getResourceAsStream(inputName);
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final BufferedReader br = new BufferedReader(inputStreamReader);
+            String line;
 
-    private class Server {
+            // First line
+            line = br.readLine();
+            String[] args = line.split(" ");
+
+            problem.r = Integer.valueOf(args[0]);
+            problem.racks = new ArrayList<Rack>(problem.r);
+            problem.s = Integer.valueOf(args[1]);
+            problem.u = Integer.valueOf(args[2]);
+            problem.p = Integer.valueOf(args[3]);
+            problem.groups = new ArrayList<Group>(problem.p);
+            problem.m = Integer.valueOf(args[4]);
+            problem.servers = new ArrayList<Server>(problem.m);
+
+            // Create Racks
+            for (int i = 0; i < problem.r; i++) {
+                final Rack rack = new Rack(i, problem.s);
+                problem.racks.add(rack);
+            }
+
+            // Create Groups
+            for (int i = 0; i < problem.p; i++) {
+                final Group group = new Group(i);
+                problem.groups.add(group);
+            }
+
+            // Read unavailability
+            for (int i = 0; i < problem.u; i++) {
+                line = br.readLine();
+                args = line.split(" ");
+                final int rackNumber = Integer.valueOf(args[0]);
+                final int unavailbilityPosition = Integer.valueOf(args[1]);
+
+                final Rack rack = problem.racks.get(rackNumber);
+                rack.markAsUnavailable(unavailbilityPosition);
+            }
+
+            // Read servers
+            for (int i = 0; i < problem.m; i++) {
+                line = br.readLine();
+                args = line.split(" ");
+                final int size = Integer.valueOf(args[0]);
+                final int capacity = Integer.valueOf(args[1]);
+
+                final Server server = new Server(i, capacity, size);
+                problem.servers.add(server);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return problem;
+    }
+
+    private static class Problem {
+        public List<Server> servers;
+        public List<Rack> racks;
+        public List<Group> groups;
+        public int r, s, u, p, m;
+
+    }
+
+
+    private static class Server {
         public int capacity = 0;
         public int size = 0;
         public int id = -1;
@@ -100,9 +175,17 @@ public final class Launcher {
             this.capacity = capacity;
             this.size = size;
         }
+
+        @Override
+        public String toString() {
+            // TODO use that for result
+            // return id + " " + rackNumber + " " + rackPosition + " " + groupId;
+            return "Server id: " + id + "  capacity: " + capacity + "  size: " + size + "  rackNumber: " + rackNumber + "  rackPosition:" + rackPosition + "  groupId:" + groupId;
+
+        }
     }
 
-    private class Rack {
+    private static class Rack {
         public int id = -1;
         public boolean[] availability;
         public ArrayList<Server> servers = new ArrayList<Server>();
@@ -128,9 +211,14 @@ public final class Launcher {
             server.rackPosition = position;
             servers.add(server);
         }
+
+        @Override
+        public String toString() {
+            return "Rack id: " + id + "  capacityTotal: " + capacityTotal + "  freeSize: " + freeSize + "  availability: " + Arrays.toString(availability);
+        }
     }
 
-    private class Group {
+    private static class Group {
         public int id = -1;
         ArrayList<Server> servers = new ArrayList<Server>();
         public int sizeTotal;
@@ -145,6 +233,11 @@ public final class Launcher {
             capacityTotal += server.capacity;
             server.groupId = id;
             servers.add(server);
+        }
+
+        @Override
+        public String toString() {
+            return "Group id: " + id + "  sizeTotal: " + sizeTotal + "  capacityTotal: " + capacityTotal;
         }
     }
 }
