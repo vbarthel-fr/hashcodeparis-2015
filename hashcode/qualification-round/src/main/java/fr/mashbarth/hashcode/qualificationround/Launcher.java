3aa04f2ac5bbeee1a0ef71bbb2fe114b0f1538bd
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2015-03-12 07:50:52 +0100

    Fix package typo. Add a Main-Class attribut in the manifest. Use a resource instead of a file for the input.

--- hashcode/qualification-round/src/main/java/fr/mashbarth/hashcode/qualificationround/Launcher.java
+++ hashcode/qualification-round/src/main/java/fr/mashbarth/hashcode/qualificationround/Launcher.java
@@ -1,27 +1,23 @@
-package fr.mashbarth.hascode.qualificationround;
+package fr.mashbarth.hashcode.qualificationround;
 
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class Launcher {
 
-    private static final String INPUT_DIRECTORY = "input";
-
-    private static final String OUTPUT_DIRECTORY = "output";
-
-
     public static void main(String[] args) {
         System.out.println("#### Qualification Round ####");
 
         // Example of reading a file.
-        readFile(INPUT_DIRECTORY + "/test.txt");
+        readResource("/test-resources.txt");
 
         // Example of writing a list of object to a file.
         final List<String> strings = new ArrayList<String>();
@@ -30,21 +26,22 @@ public final class Launcher {
         strings.add("a");
         strings.add("small");
         strings.add("example !");
-        writeFile(OUTPUT_DIRECTORY + "/test.txt", strings);
+        writeFile("test-result.txt", strings);
     }
 
 
     /**
-     * Read a file line by line.
-     * A simple example of reading a file line by line.
+     * Read a resource line by line.
+     * A simple example of reading a resource line by line.
      *
-     * @param fileName the name of the file.
+     * @param resourceName the name of the resource.
      */
-    public static void readFile(String fileName) {
+    public static void readResource(String resourceName) {
         try {
-            final File inputFile = new File(fileName);
-            final FileReader fileReader = new FileReader(inputFile);
-            final BufferedReader br = new BufferedReader(fileReader);
+
+            final InputStream inputStream = resourceName.getClass().getResourceAsStream(resourceName);
+            final InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            final BufferedReader br = new BufferedReader(inputStreamReader);
             String line;
 
             while ((line = br.readLine()) != null) {
